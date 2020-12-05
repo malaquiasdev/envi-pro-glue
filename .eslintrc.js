6631abf2eaa9899e463f69d034e4db982159428f
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2020-12-05 20:51:45 -0300

    install dev dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier", "jest"],
+  rules: {
+    "arrow-body-style": "off",
+    "function-paren-newline": "off",
+    "prettier/prettier": ["error"],
+    complexity: ["error", 5],
+    "max-lines": ["error", { max: 50, skipComments: true }],
+    "max-statements": ["warn", { max: 10 }],
+    "max-statements-per-line": ["warn", { max: 1 }],
+    "max-nested-callbacks": ["warn", { max: 2 }],
+    "max-depth": ["warn", { max: 2 }],
+    "max-len": ["warn", 120, { ignoreUrls: true }],
+    "max-params": ["warn", 3],
+  },
+  globals: {
+    describe: true,
+    expect: true,
+    it: true,
+    jest: true,
+  },
+  env: {
+    es6: true,
+    jest: true,
+    node: true,
+  },
+};
---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  watchPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  testMatch: ["<rootDir>/src/**/*.test.js"],
+  timers: "fake",
+  clearMocks: true,
+  resetMocks: false,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/packages/*.js"],
+  coverageDirectory: "./build/cov",
+  coverageReporters: ["lcov"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
