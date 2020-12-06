871e934ae4cedcd8afbf23efdcf2048575e65d2f
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2020-12-06 00:14:08 -0300

    refactor: remove the dynamoose batch function because we are had some problems

--- src/functions/collect-vacant/handler.js
+++ src/functions/collect-vacant/handler.js
@@ -3,13 +3,17 @@ const fetchData = require('../../libs/http/fetch-data');
 const SummaryVacantModel = require('../../models/Vacant');
 const crawlerSummaryVacantPage = require('./crawler-summary-vacant-page');
 
-async function handlerCollectVacant(event, _) {
+async function handlerCollectVacant(event) {
+  let vacants = null;
   try {
-    const vacants = await crawlerSummaryVacantPage(event, { fetchData });
-    await SummaryVacantModel.batchPut(vacants, { overwrite: false });
+    vacants = await crawlerSummaryVacantPage(event, { fetchData });
+    for (const vacant of vacants) {
+      await SummaryVacantModel.create(vacant);
+    }
   } catch (error) {
     logger.info(event, 'event');
-    logger.error(error, 'error');
+    logger.info(vacants, 'vacants');
+    throw error;
   }
 }
 
