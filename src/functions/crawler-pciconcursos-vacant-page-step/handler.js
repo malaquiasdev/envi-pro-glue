cdeecf541e3789e56c793139ed99e7d917b69936
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2021-01-30 14:39:13 -0300

    update: save vacant just if have some evenDate valid

--- src/functions/crawler-pciconcursos-vacant-page-step/handler.js
+++ src/functions/crawler-pciconcursos-vacant-page-step/handler.js
@@ -9,13 +9,18 @@ async function handlerCrawlerPciconcursosVacantPageStep(event) {
   try {
     const VacantModel = createVacantModel(config.tableName);
     const rawData = await fetchData(`${config.baseUrl}/${event.category}`);
-    await VacantModel.create({
+    const result = parseRawDataToVacantModel(rawData).filter(vacant => {
+      return new Date(vacant.eventDate) > new Date();
+    });
+    const vacant = {
       id: uuid.v4(),
       category: event.category,
-      result: parseRawDataToVacantModel(rawData).filter(vacant => {
-        return new Date(vacant.eventDate) > new Date();
-      })
-    });
+      result
+    };
+    if (result) {
+      await VacantModel.create(vacant);
+    }
+    return vacant;
   } catch (error) {
     logger.error(error);
     throw error;
