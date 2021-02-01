b8c50dd2ccf9d7abacdece82cec63fc5cbbcbe58
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2021-02-01 20:46:56 -0300

    Refactor get vacant event date function (#11)
    * refactor and fix
    
    * refector: eventDate to date

--- src/functions/crawler-pciconcursos-vacant-page-step/handler.js
+++ src/functions/crawler-pciconcursos-vacant-page-step/handler.js
@@ -10,7 +10,7 @@ async function handlerCrawlerPciconcursosVacantPageStep(event) {
     const VacantModel = createVacantModel(config.tableName);
     const rawData = await fetchData(`${config.baseUrl}/${event.category}`);
     const result = parseRawDataToVacantModel(rawData).filter(vacant => {
-      return new Date(vacant.eventDate) > new Date();
+      return vacant.eventDate > new Date();
     });
     const vacant = {
       id: uuid.v4(),
