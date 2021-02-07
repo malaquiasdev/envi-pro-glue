212d08c5fc2edd20f01242a7871114ad96f96e1c
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2021-02-07 10:47:25 -0300

    Feat function export csv file (#12)
    * update: save vacant just if have some evenDate valid
    
    * feat: start export csv file
    
    * feat: start export csv file
    
    * fix: put file in s3 lib usage
    
    * merged
    
    * change configurations
    
    * fix: date pattern in the csv file

---
+++ src/functions/export-data-to-csv-file-step/handler.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+const logger = require('pino')();
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const config = require('./config');
+const createVacantModel = require('../../models/Vacant');
+const parseDataToCSV = require('./parse-data-to-csv');
+
+const s3 = new AWS.S3();
+
+async function handlerExportDataToCSVFileStep() {
+  try {
+    const VacantModel = createVacantModel(config.tableName);
+    const data = await VacantModel.scan().exec();
+    await parseDataToCSV(data);
+    fs.readFile('/tmp/data.csv', 'utf8', (err, res) => {
+      if (err) {
+        logger.error(err, 'err');
+      }
+      const params = {
+        Bucket: config.bucketName,
+        Key: `vacants-${new Date().getTime()}.csv`,
+        ACL: 'public-read',
+        Body: res,
+        ContentType: 'text/csv'
+      };
+      return s3.putObject(params, (s3Err, result) => {
+        if (s3Err) {
+          throw s3Err;
+        } else {
+          return { redirectUri: result.Location };
+        }
+      });
+    });
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+}
+
+module.exports = { handlerExportDataToCSVFileStep };
