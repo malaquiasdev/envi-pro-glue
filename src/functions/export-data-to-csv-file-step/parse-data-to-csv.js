83df0212dde3eab14fefba3cb281d1ca5182d454
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2021-02-07 10:19:51 -0300

    merged

--- src/functions/export-data-to-csv-file-step/parse-data-to-csv.js
+++ src/functions/export-data-to-csv-file-step/parse-data-to-csv.js
@@ -1,13 +1,7 @@
 /* eslint-disable no-restricted-syntax */
-<<<<<<< HEAD
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 async function parseDataToCSV(array) {
-=======
-const createCsvStringifier = require('csv-writer').createObjectCsvStringifier;
-
-function parseDataToCSV(array) {
->>>>>>> 7df176f0ce0c3f9349ba1f5c32f1cae4885165ec
   const flatData = [];
   for (const data of array) {
     for (const subItem of data.result) {
@@ -18,7 +12,6 @@ function parseDataToCSV(array) {
       flatData.push(newItem);
     }
   }
-<<<<<<< HEAD
   const csv = createCsvWriter({
     path: '/tmp/data.csv',
     header: [
@@ -31,19 +24,6 @@ function parseDataToCSV(array) {
   });
   await csv.writeRecords(flatData);
   return '/tmp/data.csv';
-=======
-  const csvStringifier = createCsvStringifier({
-    header: [
-      { id: 'category', title: 'CATEGORY' },
-      { id: 'link', title: 'LINK' },
-      { id: 'uf', title: 'UF' },
-      { id: 'title', title: 'TITLE' },
-      { id: 'eventDate', title: 'EVENT_DATE' }
-    ]
-  });
-
-  return csvStringifier.stringifyRecords(flatData);
->>>>>>> 7df176f0ce0c3f9349ba1f5c32f1cae4885165ec
 }
 
 module.exports = parseDataToCSV;
