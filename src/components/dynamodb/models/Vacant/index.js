d2e985df6d82381ec4b808df8a69d90ac254ff34
mateus.malaquias.glo <mateus.malaquias@globalhitss.com.br>
2021-04-24 18:58:55 -0300

    delete dynamodb component with dynamoose lib

--- src/components/dynamodb/models/Vacant/index.js
+++
@@ -1,8 +0,0 @@
-const dynamoose = require('dynamoose');
-const createDynamodbInstance = require('../../create-instance');
-const createVacantSchema = require('./schema');
-
-function createVacantModel(tableName) {
-  return createDynamodbInstance(tableName, createVacantSchema(dynamoose));
-}
-module.exports = createVacantModel;
